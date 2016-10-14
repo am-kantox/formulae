e69caee0a064a539bef43cb5219ca34cd621f136
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2016-10-14 19:32:50 +0200

    Friday evening commit.

--- lib/errors/runner.ex
+++ lib/errors/runner.ex
@@ -4,7 +4,7 @@ defmodule Formulae.Runner do
   def exception(value) do
     {what, how} = value[:error]
     message = "Formula failed to run (#{what}): #{how}."
-    %Formulae.Runner{formula: value[:formula], message: message}
+    %Formulae.Runner{formula: value[:formula], message: message, error: value[:error]}
   end
 
 end
--- lib/formulae.ex
+++ lib/formulae.ex
@@ -3,6 +3,28 @@ defmodule Formulae do
   ##############################################################################
 
   @doc ~S"""
+  Revalidates the formula with bindings given.
+
+  ## Examples
+
+      iex> "a > 5" |> Formulae.check([a: 6])
+      true
+
+      iex> "a > 5" |> Formulae.check([a: 3])
+      false
+
+      iex> "a > 5" |> Formulae.check
+      false
+  """
+  def check(string, bindings \\ []) do
+    try do
+      Formulae.evaluate(string, bindings)
+    rescue
+      Formulae.Runner -> false
+    end
+  end
+
+  @doc ~S"""
   Returns a normalized (`{:<, "sumOaSTS3E14CSMSb", 0}`) representation
   for the formula given.
 
@@ -10,12 +32,45 @@ defmodule Formulae do
 
       iex> "(temp - time * 4) > speed / 3.14" |> Formulae.normalize
       {:>, {"tempSMStimeSTS4SMSspeedSDS3E14", "temp - time * 4 - speed / 3.14"}, 0}
+
       iex> "hello < 3.14" |> Formulae.normalize
       {:<, {"hello", "hello"}, 3.14}
   """
   def normalize(string) when is_binary(string) do
     {operation, _, [formula, value]} = string |> unit
     {operation, formula |> rigid, value}
+  end
+
+  @binding_finder ~r|undefined function (\w+)/0|
+
+  @doc ~S"""
+  Guesses the binding this formula requires.
+  FIXME: probably, more sophisticated way would be to analyze Macro.traverse
+
+  ## Examples
+
+      iex> "a > 5" |> Formulae.bindings?
+      ~w|a|a
+
+      iex> ":math.sin(a / (3.14 * b)) > c" |> Formulae.bindings?
+      ~w|a b c|a
+  """
+  def bindings?(string, bindings \\ []) do
+    try do
+      Formulae.evaluate(string, bindings)
+      bindings |> Keyword.keys
+    rescue
+      e in Formulae.Runner ->
+        case e.error do
+          {:compile, message} ->
+            neu = @binding_finder
+                    |> Regex.run(message, capture: :all_but_first)
+                    |> Enum.map(fn e -> {String.to_atom(e), 1} end)
+            bindings?(string, bindings ++ neu)
+          _ ->
+            raise(e)
+        end
+    end
   end
 
   ##############################################################################
@@ -63,6 +118,12 @@ defmodule Formulae do
 
       iex> Formulae.unit("3 a > a + 2")
       ** (Formulae.Syntax) Formula [3 a > a + 2] syntax is incorrect (parsing): syntax error before: “a”.
+
+      iex> Formulae.unit("a + 2 = 3")
+      {:==, [], [{:+, [line: 1], [{:a, [line: 1], nil}, 2]}, 3]}
+
+      iex> Formulae.unit(~S|a = "3"|)
+      {:==, [], [{:a, [line: 1], nil}, "3"]}
   """
   def unit(input, env \\ []) when is_binary(input) do
     case Code.string_to_quoted(input) do
@@ -72,6 +133,7 @@ defmodule Formulae do
       {:ok, {:<, _, [lh, rh]}} when is_integer(rh) or is_float(rh) -> {:<, env, [lh, rh]}
       {:ok, {:<, _, [lh, rh]}} -> {:<, env, [(quote do: unquote(lh) - unquote(rh)), 0]}
 
+      {:ok, {:=, _, [lh, rh]}} when is_integer(rh) or is_float(rh) or is_binary(rh) or is_boolean(rh) or is_nil(rh) -> {:==, env, [lh, rh]}
       {:ok, {:=, _, [lh, rh]}} -> {:==, lh - rh}
 
       {:ok, {op, _, _}} -> raise(Formulae.Syntax, formula: input, error: {:operation, double_quote(op)})
@@ -87,25 +149,55 @@ defmodule Formulae do
 
       iex> Formulae.evaluate(Formulae.unit("3 > 2"))
       true
+
       iex> Formulae.evaluate(Formulae.unit("3 < 2"))
       false
+
       iex> Formulae.evaluate(Formulae.unit("a < 2"), [a: 1])
       true
+
       iex> Formulae.evaluate(Formulae.unit("a > 2"), [a: 1])
       false
+
+      # This test issues a warning about “variable "a" does not exist and is being expanded to "a()"”
       iex> Formulae.evaluate(Formulae.unit("a < 2"), [])
       ** (Formulae.Runner) Formula failed to run (compile): undefined function a/0.
+
+      iex> Formulae.evaluate(Formulae.unit("a + 2 = 3"), [a: 1])
+      true
+
+      iex> Formulae.evaluate(Formulae.unit("a + 2 = 3"), [a: 2])
+      false
+
+      iex> Formulae.evaluate(Formulae.unit(~S|a = "3"|), [a: "3"])
+      true
+
+      iex> Formulae.evaluate(Formulae.unit(~S|a = "3"|), [a: 3])
+      false
+
+      iex> Formulae.evaluate(Formulae.unit(~S|a = "3"|), [a: "hello"])
+      false
+
+      iex> Formulae.evaluate("a + 2 = 3", [a: 2])
+      false
+
+      iex> Formulae.evaluate(~S|a = "3"|, [a: "3"])
+      true
   """
-  def evaluate(quoted, binding \\ [], opts \\ []) do
+  def evaluate(input, binding \\ [], opts \\ [])
+  def evaluate(input, binding, opts) when is_tuple(input) do
     try do
-      case Code.eval_quoted(quoted, binding, opts) do
+      case Code.eval_quoted(input, binding, opts) do
         {false, ^binding} -> false
         {true,  ^binding} -> true
-        other             -> raise(Formulae.Runner, formula: quoted, error: {:weird, inspect(other)})
+        other             -> raise(Formulae.Runner, formula: input, error: {:weird, inspect(other)})
       end
     rescue
-      e in CompileError -> raise(Formulae.Runner, formula: quoted, error: {:compile, e.description})
+      e in CompileError -> raise(Formulae.Runner, formula: input, error: {:compile, e.description})
     end
+  end
+  def evaluate(input, binding, opts) when is_binary(input) do
+    evaluate(input |> unit, binding, opts)
   end
 
   @doc ~S"""
@@ -115,10 +207,13 @@ defmodule Formulae do
 
       iex> Formulae.rigid?("hello")
       true
+
       iex> Formulae.rigid?("hello world!")
       false
+
       iex> Formulae.rigid?("sum0")
       true
+
       iex> Formulae.rigid?("sum(3.14 * 42)")
       false
   """
@@ -138,6 +233,7 @@ defmodule Formulae do
 
     iex> "sum(a * 3.14) - b" |> Formulae.rigid!
     "sumOaSTS3E14CSMSb"
+
     iex> "sum(a * 3.14) - b ± 3" |> Formulae.rigid!
     ** (Formulae.Syntax) Formula [sum(a * 3.14) - b ± 3] syntax is incorrect (symbols): NYI.
 
@@ -168,7 +264,7 @@ defmodule Formulae do
   """
   def unrigid!(var) when is_binary(var) do
     unless var |> rigid?(false), do: raise(Formulae.Syntax, formula: var, error: {:not_rigid, "NYI"})
-    Regex.replace(@regexps |> List.last, var, fn key, _ -> @var_to_fun[key] end)
+    if var |> rigid?(true), do: var, else: Regex.replace(@regexps |> List.last, var, fn key, _ -> @var_to_fun[key] end)
   end
 
   ##############################################################################
