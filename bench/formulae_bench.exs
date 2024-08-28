3d59156ffb6ca23c0673df679cc64c9855b7a03f
Aleksei Matiushkin <am@ambment.cat>
2024-08-28 11:20:25 +0200

    Improved benchmarks

--- bench/formulae_bench.exs
+++ bench/formulae_bench.exs
@@ -5,18 +5,26 @@ defmodule Formulae.Bench do
   @long Enum.to_list(1..1_000)
 
   bench "generate modules (function)" do
-    for i <- @short, do: Formulae.compile("a > #{i}")
+    for i <- @short, do: Formulae.compile("a > #{i}", imports: :none)
   end
 
   bench "generate modules (guard)" do
-    for i <- @short, do: Formulae.compile("b > #{i}", evaluator: :guard)
+    for i <- @short, do: Formulae.compile("b > #{i}", imports: :none, evaluator: :guard)
   end
 
-  bench "check (function)", [f: Formulae.compile("ff > 500")] do
+  bench "check (function)", [f: Formulae.compile("ff > 500", imports: :none)] do
     for i <- @long, do: f.eval.(ff: i)
   end
 
-  bench "check (guard)", [f: Formulae.compile("fg > 500")] do
+  bench "check (anonymous function)", [f_eval: fn ff -> ff > 500 end] do
+    for i <- @long, do: f_eval.(i)
+  end
+
+  bench "check (guard)", [f: Formulae.compile("fg > 500", imports: :none)] do
     for i <- @long, do: f.eval.(fg: i)
+  end
+
+  bench "check (guard in anonymous function)", [f_eval: fn fg when fg > 500 -> true; _ -> false end] do
+    for i <- @long, do: f_eval.(i)
   end
 end
